c2a0b56059a1928ed402a5e35169192cc482fb89
Your Name <you@example.com>
2019-07-30 20:02:12 -0500

    Se agregó owner al componente car list

--- src/app/car-list/car-list.component.ts
+++ src/app/car-list/car-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CarService } from '../shared/car/car.service';
 import { GiphyService } from '../shared/giphy/giphy.service';
+import { OwnerService } from '../shared/owner/owner.service'
 
 @Component({
   selector: 'app-car-list',
@@ -10,13 +11,30 @@ import { GiphyService } from '../shared/giphy/giphy.service';
 export class CarListComponent implements OnInit {
   cars: Array<any>;
 
-  constructor(private carService: CarService, private giphyService: GiphyService) { }
+  constructor(private carService: CarService, private ownerService: OwnerService, private giphyService: GiphyService) { }
 
   ngOnInit() {
+
+
+
     this.carService.getAll().subscribe(data => {
       this.cars = data;
       for (const car of this.cars) {
         this.giphyService.get(car.name).subscribe(url => car.giphyUrl = url);
+        if (car.ownerDni) {
+          console.log(car.ownerDni)
+          this.ownerService.getByDni(car.ownerDni).subscribe((owner: any) => {
+
+            if(owner[0]){
+              car.ownerName = owner[0].name
+              console.log(car.ownerName)
+            }
+
+          })
+
+        }
+
+
       }
     });
   }
